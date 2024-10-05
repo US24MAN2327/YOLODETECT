ba45a048a7817ae80ac99059786220887e02d039
Muhammad Usman <108360059+US24MAN2327@users.noreply.github.com>
2024-10-05 22:51:07 +0500

    Update detect.py

--- detect.py
+++ detect.py
@@ -13,11 +13,14 @@ def process_image(image):
     # Convert PIL Image to numpy array
     img_array = np.array(image)
     
+    # Check if the image has an alpha channel (4 channels)
+    if img_array.shape[-1] == 4:
+        # Convert RGBA to RGB
+        img_array = img_array[:, :, :3]
+    
     # Ensure the image is in RGB format
     if len(img_array.shape) == 2:  # Grayscale
         img_array = cv2.cvtColor(img_array, cv2.COLOR_GRAY2RGB)
-    elif img_array.shape[-1] == 4:  # RGBA
-        img_array = cv2.cvtColor(img_array, cv2.COLOR_RGBA2RGB)
     elif img_array.shape[-1] == 3:  # RGB
         img_array = cv2.cvtColor(img_array, cv2.COLOR_BGR2RGB)
     
@@ -28,9 +31,7 @@ def process_image(image):
     res_plotted = results[0].plot()
     return Image.fromarray(res_plotted)
 
-def main():
-    st.title("YOLOv8 Object Detection")
-    
+def webcam_access():
     # JavaScript to capture webcam image
     js_code = """
     <script>
@@ -82,6 +83,28 @@ def main():
         if st.button('Detect Objects'):
             processed_image = process_image(st.session_state.captured_image)
             st.image(processed_image, caption='Processed Image', use_column_width=True)
+
+def file_upload_access():
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+        
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+        
+        if st.button('Detect Objects'):
+            processed_image = process_image(image)
+            st.image(processed_image, caption='Processed Image', use_column_width=True)
+
+def main():
+    st.title("YOLOv8 Object Detection")
+    
+    # Sidebar for selecting input method
+    input_method = st.sidebar.radio("Select Input Method", ["Webcam", "File Upload"])
+    
+    if input_method == "File Upload":
+        file_upload_access()
+    elif input_method == "Webcam":
+        webcam_access()
 
 if __name__ == "__main__":
     main()
